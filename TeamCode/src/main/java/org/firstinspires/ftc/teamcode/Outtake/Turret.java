0b96fd1e804d1b571d12c277cbbb9cd6476669ff
Satvik Palagummi <satvik.palagummi@gmail.com>
2025-10-30 15:33:08 -0500

    Turret class made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake/Turret.java
@@ -1,8 +1,20 @@
 package org.firstinspires.ftc.teamcode.Outtake;
 
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class Turret {
-    private Servo turret;
-    private double theta;
+    private Servo turret = null;
+    public void setTurret(Servo turret){
+        this.turret = turret;
+    }
+    public void setPos(double pos){
+        turret.setPosition(pos);
+    }
+    public double getTurret(){
+        return turret.getPosition();
+    }
+
+
 }
